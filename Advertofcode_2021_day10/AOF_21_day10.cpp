cb10a70cfbc0f0dc1640d65ef591628de06c736f
0SasikantH0 <31877220+0SasikantH0@users.noreply.github.com>
2022-01-20 22:23:23 +0530

    Pushing open braces
    - Pushing open braces and printing the stack.

--- Advertofcode_2021_day10/AOF_21_day10.cpp
+++ Advertofcode_2021_day10/AOF_21_day10.cpp
@@ -1,20 +1,35 @@
 #include<stdio.h>
 
 //Basic character stack DS
-char stack[100],stactSize,top=-1;
+char stack[100],stackSize,top=-1;
 void push(char openBrace);
 char pop();
 void show();
 
 int main()
 {
+    char cEachChar;
+    printf("Enter sequence \n");
+    stackSize = 98;
+    scanf("%c",&cEachChar);
+    while (cEachChar != '\n') {
+      if(cEachChar == '[' || cEachChar == '{' || cEachChar == '('|| cEachChar == '<')
+      {
+          push(cEachChar);
+      }
+      else
+      {
+      }
+      scanf("%c",&cEachChar);
+    }
+    show();
     return 0;
 }
 
 //Stack functions
 void push (char openBrace)
 {
-    if (top == stactSize )
+    if (top == stackSize )
     printf("\n Overflow");
     else
     {
