0046c8b0c25000e146ab56941def3066a9c1d7a4
0SasikantH0 <31877220+0SasikantH0@users.noreply.github.com>
2022-01-20 22:13:55 +0530

    Added Basic stack implementation
    - Added stack implementation on char array as required by the problem.

--- Advertofcode_2021_day10/AOF_21_day10.cpp
+++ Advertofcode_2021_day10/AOF_21_day10.cpp
@@ -1,6 +1,50 @@
 #include<stdio.h>
 
-void main()
-{
+//Basic character stack DS
+char stack[100],stactSize,top=-1;
+void push(char openBrace);
+char pop();
+void show();
 
+int main()
+{
+    return 0;
+}
+
+//Stack functions
+void push (char openBrace)
+{
+    if (top == stactSize )
+    printf("\n Overflow");
+    else
+    {
+        top = top +1;
+        stack[top] = openBrace;
+    }
+}
+
+char pop ()
+{
+    char brace;
+    if(top == -1)
+    printf("Underflow");
+    else
+    {
+      brace = stack[top];
+      top = top -1;
+    }
+    return brace;
+}
+
+void show()
+{
+    int i;
+    for (i=top;i>=0;i--)
+    {
+        printf("%c\n",stack[i]);
+    }
+    if(top == -1)
+    {
+        printf("Stack is empty");
+    }
 }
